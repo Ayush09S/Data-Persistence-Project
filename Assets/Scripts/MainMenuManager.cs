363c0a42c6751b0ffac5aab680ba54018812866f
Thunderball5892 <157115726+Ayush09S@users.noreply.github.com>
2024-08-31 14:34:40 +1000

    Minimal Update
    - Recent and High score integer
    - Persistence Script creation

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -8,9 +8,12 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     public TMP_Text nameText;
-    public TMP_InputField nameInput;
+    public TMP_InputField nameInput; // Use to save name for score in Persistence Script
     public TMP_Text recentScoreText;
     public TMP_Text highScoreText;
+
+    public int recentScore;
+    public int highScore;
 
     // Start is called before the first frame update
     void Start()
